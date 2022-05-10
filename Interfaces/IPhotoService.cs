a93f956521e264b6713a057965e5e0d2010226cb
Jennifer Oliseyenum <jennifer.oliseyenum@fintraksoftware.com>
2022-05-10 04:25:04 -0700

    Initial

---
+++ Interfaces/IPhotoService.cs
@@ -0,0 +1,16 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingAPI.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> UploadPhotoAsync(IFormFile photo);
+
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
